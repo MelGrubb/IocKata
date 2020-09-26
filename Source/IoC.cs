b264474dd6b8b0be16096ba9f965d7d1311521ab
Mel Grubb <melgrubb@live.com>
2020-09-26 17:22:17 -0400

    Step7 Complete

--- Source/IoC.cs
+++ Source/IoC.cs
@@ -7,96 +7,62 @@ namespace IocKata
 {
     public static class IoC
     {
-        private enum DependencyType
-        {
-            Instance,
-            Delegate,
-            Dynamic,
-        }
-
         private static readonly Dictionary<Type, (object value, DependencyType dependencyType, bool isSingleton)> Dependencies = new Dictionary<Type, (object value, DependencyType dependencyType, bool isSingleton)>();
 
-        public static void Register<T>(T instance)
-        {
-            Dependencies[typeof(T)] = (instance, DependencyType.Instance, true);
-        }
+        public static void Register<T>(T instance) => Dependencies[typeof(T)] = (instance, DependencyType.Instance, true);
 
-        public static void Register<T>(Func<object> func, bool isSingleton = false)
-        {
-            Dependencies[typeof(T)] = (func, DependencyType.Delegate, isSingleton);
-        }
+        public static void Register<T>(Func<object> func, bool isSingleton = false) => Dependencies[typeof(T)] = (func, DependencyType.Delegate, isSingleton);
 
-        public static void Register<T1, T2>(bool isSingleton = false)
-        {
-            Dependencies[typeof(T1)] = (typeof(T2), DependencyType.Dynamic, isSingleton);
-        }
+        public static void Register<T1, T2>(bool isSingleton = false) => Dependencies[typeof(T1)] = (typeof(T2), DependencyType.Dynamic, isSingleton);
 
         public static void Register(Assembly assembly)
         {
-            var types = assembly.GetTypes().Where(t => t.GetInterfaces().Any(i => i.Name == $"I{t.Name}"));
-            foreach (var type in types)
-            {
+            foreach (var type in assembly.GetTypes().Where(t => t.GetInterfaces().Any(i => i.Name == $"I{t.Name}")))
                 Dependencies[type.GetInterface($"I{type.Name}")] = (type, DependencyType.Dynamic, false);
-            }
         }
 
-        public static T Resolve<T>()
-        {
-            return (T) Resolve(typeof(T));
-        }
+        public static T Resolve<T>() => (T) Resolve(typeof(T));
 
         private static object Resolve(Type type)
         {
             var dependency = Dependencies[type];
+            object value = null;
 
-            if (dependency.dependencyType == DependencyType.Instance)
+            if (dependency.dependencyType == DependencyType.Instance) return dependency.value;
+
+            if (dependency.dependencyType == DependencyType.Delegate)
             {
-                return dependency.value;
-            }
-            else if (dependency.dependencyType == DependencyType.Delegate)
-            {
-                var value = ((Func<object>) dependency.value).Invoke();
-                if (dependency.isSingleton)
-                {
-                    Dependencies[type] = (value, DependencyType.Instance, false);
-                }
+                value = ((Func<object>) dependency.value).Invoke();
+                if (dependency.isSingleton) Dependencies[type] = (value, DependencyType.Instance, false);
 
                 return value;
             }
-            else
-            {
-                var concreteType = (Type) dependency.value;
-                var constructorInfo = concreteType.GetConstructors()
-                    .OrderByDescending(o => (o.GetCustomAttributes(typeof(InjectionConstructorAttribute), false).Any()))
-                    .ThenByDescending(o => (o.GetParameters().Length)).First();
-                var parameterInfos = constructorInfo.GetParameters();
 
-                if (parameterInfos.Length == 0)
-                {
-                    return Activator.CreateInstance((Type)dependency.value);
-                }
-                else
-                {
-                    var parameters = new List<object>(parameterInfos.Length);
-                    foreach (ParameterInfo parameterInfo in parameterInfos)
-                    {
-                        parameters.Add(Resolve(parameterInfo.ParameterType));
-                    }
-                    var value = constructorInfo.Invoke(parameters.ToArray());
+            var concreteType = (Type) dependency.value;
+            var constructorInfo = concreteType.GetConstructors()
+                .OrderByDescending(o => o.GetCustomAttributes(typeof(InjectionConstructorAttribute), false).Any())
+                .ThenByDescending(o => o.GetParameters().Length).First();
+            var parameterInfos = constructorInfo.GetParameters();
 
-                    if (dependency.isSingleton)
-                    {
-                        Dependencies[type] = (value, DependencyType.Instance, false);
-                    }
-                
-                    return value;
-                }
-            }
+            if (parameterInfos.Length == 0)
+                return Activator.CreateInstance((Type) dependency.value);
+
+            var parameters = new List<object>(parameterInfos.Length);
+            foreach (var parameterInfo in parameterInfos) parameters.Add(Resolve(parameterInfo.ParameterType));
+            value = constructorInfo.Invoke(parameters.ToArray());
+
+            if (dependency.isSingleton) Dependencies[type] = (value, DependencyType.Instance, false);
+
+            return value;
         }
 
-        public static void Reset()
+        public static void Reset() => Dependencies.Clear();
+
+        private enum DependencyType
         {
-            Dependencies.Clear();
+            Instance,
+            Delegate,
+            Dynamic
         }
     }
 }
