80eb1342b9c3b763569485faea91c6a28388635f
Mel Grubb <melgrubb@live.com>
2020-09-27 09:21:28 -0400

    Reset main branch to starting solution

--- Source/IoC.cs
+++ Source/IoC.cs
@@ -1,20 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace IocKata
+﻿namespace IocKata
 {
     public static class IoC
     {
-        private static readonly Dictionary<Type, object> Dependencies = new Dictionary<Type, object>();
-
-        public static void Register<T>(T instance)
-        {
-            Dependencies[typeof(T)] = instance;
-        }
-
-        public static T Resolve<T>()
-        {
-            return (T) Dependencies[typeof(T)];
-        }
     }
 }
